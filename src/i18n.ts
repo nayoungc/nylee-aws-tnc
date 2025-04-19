32f246747e19db5eb9c2e62831f8045c37ca1eb2
Nayoung <naoung2409@gmail.com>
2025-04-19 10:03:44 +0000

    login

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,39 +2,27 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-// 영어와 한국어 번역 리소스
-import translationEN from './translations/en.json';
-import translationKO from './translations/ko.json';
-
-// 리소스 구성
-const resources = {
-  en: {
-    translation: translationEN
-  },
-  ko: {
-    translation: translationKO
-  }
-};
+import Backend from 'i18next-http-backend';
 
 i18n
-  // 언어 감지 기능 사용
+  .use(Backend)
   .use(LanguageDetector)
-  // React i18next 모듈 초기화
   .use(initReactI18next)
-  // 초기화 옵션
   .init({
-    resources,
     fallbackLng: 'en',
     debug: process.env.NODE_ENV === 'development',
-    
+    supportedLngs: ['en', 'ko'],
     interpolation: {
-      escapeValue: false, // React에서는 XSS 방지가 내장되어 있음
+      escapeValue: false,
     },
-    detection: {
-      order: ['localStorage', 'navigator'],
-      caches: ['localStorage']
-    }
+    react: {
+      useSuspense: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    returnObjects: false, // 객체 대신 문자열만 반환하도록 설정
+    returnEmptyString: true, // 빈 문자열을 undefined 대신 반환
   });
 
 export default i18n;
