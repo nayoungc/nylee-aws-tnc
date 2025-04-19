aa176c800a88742704a2dd4901f5e1235ba2f178
Nayoung <naoung2409@gmail.com>
2025-04-19 00:57:45 +0000

    login

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Amplify } from 'aws-amplify';
-import '@aws-amplify/ui-react/styles.css'; 
+import './styles/theme.css';
+import './styles/auth.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-// Amplify 설정 로드 - amplifyconfiguration.json 사용
-try {
-  const config = require('./amplifyconfiguration.json');
-  Amplify.configure(config);
-  console.log('Amplify 설정이 성공적으로 로드되었습니다.');
-} catch (e) {
-  console.error('Amplify 구성 파일을 로드할 수 없습니다:', e);
-  // 설정 파일이 없는 경우에는 빈 설정으로 초기화만 하고 경고
-  Amplify.configure({});
-  alert('인증 설정을 로드할 수 없습니다. 관리자에게 문의하세요.');
-}
+// Amplify Gen 2 설정 임포트
+import { Amplify } from 'aws-amplify'
+import amplifyconfig from './amplifyconfiguration.json'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// Amplify Gen 2 초기화
+Amplify.configure(amplifyconfig)
 
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
