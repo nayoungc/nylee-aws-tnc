8d235b17c6453aad99bb4a213922cbf2375da4ba
Nayoung <naoung2409@gmail.com>
2025-04-20 11:27:53 +0000

    AppSync

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,6 @@
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
 
-// 모든 설정을 한 번에 적용
+console.log('Amplify 설정 적용 시작');
 Amplify.configure(awsExports);
-
-// 개발 환경에서만 로깅 (선택 사항)
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Amplify 설정 완료');
-}
+console.log('Amplify 설정 완료', Amplify.getConfig());
