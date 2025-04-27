f0604409f56b75148b17e75c438b569c138abd84
Nayoung <naoung2409@gmail.com>
2025-04-27 22:48:02 +0000

    plz

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   build: {
     outDir: 'build',
   },
